1b990ab156e4487d6aad3f931ea67928769c63fb
Neo Anderson <147811206+Neon01602@users.noreply.github.com>
2025-09-21 11:23:27 +0530

    Update settings.py

--- mcras/settings.py
+++ mcras/settings.py
@@ -125,12 +125,15 @@ MEDIA_ROOT = BASE_DIR / "media"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
 
